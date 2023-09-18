e88c343c1f3538512a69043df6f3bc712be7e703
Wmulder98 <wmulder98@gmail.com>
2023-09-18 10:39:38 +0200

    Define file

---
+++ Software/StartSoftware/Pico_relay/global.h
@@ -0,0 +1,20 @@
+//Name of project:            Ralays  
+//Company:                    -
+//Date of creation:           18-09-2023
+//Date of last modification:  18-09-2023
+//Creater:                    Wouter Mulder 
+//Proccesor:                  RP2040
+//Proccesor datasheet:        https://datasheets.raspberrypi.com/rp2040/rp2040-datasheet.pdf
+
+#define RELAYCH1    21
+#define RELAYCH2    20
+#define RELAYCH3    19
+#define RELAYCH4    18
+#define RELAYCH1    17
+#define RELAYCH2    16
+#define RELAYCH3    15
+#define RELAYCH4    14
+
+#define LEDCONTROL  13
+#define BUZZER      6
+
